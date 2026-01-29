cf13fd1b4f78ae36e4e1a1558ab3245a66eab2fe
Manoj Mohatra <131569518+ManojMohatra@users.noreply.github.com>
2026-01-29 15:02:49 +0545

    Add course and enrollment models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    teacher = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="courses_taught"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="enrollments"
+    )
+
+    course=models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="enrollments"
+    )
+
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('student','course')
+
+    def __str__(self):
+        return f"{self.student.username} enrolled in {self.course.title}"
+
