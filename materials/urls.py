3c758e9409f93b6d12f8fc57e6a1b59236ecba92
raju banjade <rajubanjade838@gmail.com>
2026-02-13 09:15:12 +0545

    feat: Add URL routing for materials app
    - lecture materials view route
    - material deletion route
    - all materials dashboard route

---
+++ materials/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('lecture/<int:lecture_id>/materials/', views.lecture_materials, name='lecture_materials'),
+    path('material/<int:material_id>/delete/', views.delete_material, name='delete_material'),
+     path('all/', views.all_materials, name='all_materials'),
+]
