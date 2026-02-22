bf2fe382bfa9e2432508cc351c8b5e9d6f1c2786
Manoj Mohatra <131569518+ManojMohatra@users.noreply.github.com>
2026-02-22 14:37:03 +0545

    Refactor the css into static folder to increase reusability

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_REDIRECT_URL ='accounts:dashboard'
 LOGOUT_REDIRECT_URL = 'accounts:login'
