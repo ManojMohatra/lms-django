623d686d6ae6fdf94a65478de8e7387d722ffb3f
ManojMohatra <131569518+ManojMohatra@users.noreply.github.com>
2026-04-15 12:01:34 +0545

    add about and contact page with basic info

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 app_name = "core"
 urlpatterns = [
     path("", views.home, name="home"),
+    path('about/', TemplateView.as_view(template_name="core/about.html"), name='about'),
+    path('contact/', TemplateView.as_view(template_name="core/contact.html"), name='contact'),
 ]
